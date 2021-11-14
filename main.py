0352e81433b03f1c95dd46a7a860b7031f63b648
Caspermun <caspermunele@gmail.com>
2021-11-14 20:56:06 +0600

    first commit

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ class Person:
     def __init__(self, name, surname, age, id):
         self.name = name
         self.surname = surname
-        self.age = age
+        self.__age = age
         self.__id = id
 
     def get_fn(self):
         return self.name + ' ' + self.surname
-    def get_age(self):
-        if self.age < 1 or self.age > 100:
-            raise ValueError('Incorrect age! Type your real age.')
+
+    def set_age(self, age):
+        if age in range(1, 100):
+            self.__age = age
         else:
-            return self.age
+            raise ValueError('Incorrect age! Type your real age.')
+
+    def get_age(self):
+        self.set_age(self.__age)
+        return self.__age
+
+        # if self.age not in range(1, 100):
+        #     raise ValueError('Incorrect age! Type your real age.')
+        # else:
+        #     return self.age
+
     def get_id(self):
         return self.__id
 
